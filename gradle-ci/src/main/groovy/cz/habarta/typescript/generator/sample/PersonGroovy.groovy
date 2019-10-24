2d09e73cfcfcd424494d3fffc827f3fea74365c2
Oleg Schelykalnov <oschelicalnov@everestcollect.ru>
2019-10-24 16:36:29 +0300

    Start to test gradle-plugin

---
+++ gradle-ci/src/main/groovy/cz/habarta/typescript/generator/sample/PersonGroovy.groovy
@@ -0,0 +1,10 @@
+
+package cz.habarta.typescript.generator.sample
+
+class PersonGroovy {
+    public String name
+    public int age
+    public boolean hasChildren
+    public List<String> tags
+    public Map<String, String> emails
+}
